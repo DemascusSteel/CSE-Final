52195f30cbc0d93860710c84a48df46e898d15eb
Nathan Marble <marblestudent@gmail.com>
2022-11-29 17:25:48 -0700

    Nov 29th
    Co-authored-by: Madbrown713 <Madbrown713@users.noreply.github.com>
    Co-authored-by: Andre Regino  <regino-pan-a-i@users.noreply.github.com>
    Co-authored-by: Quackenbush, Emma <qua17003@byui.edu>

---
+++ Byui.Games/Services/ITimeServices.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Diagnostics;
+using System.Threading;
+using Byui.Games;
+
+
+namespace Timer;
+{
+    space Timer
+{
+    public class Timer
+    {
+   
+
+ Stopwatch stopwatch = new Stopwatch();
+
+    stopwatch.Start();
+    // insert some code to execute here
+    stopwatch.Stop();
+    Console.WriteLine("Time elapsed: {0:hh\\:mm\\:ss}", stopwatch.Elapsed);
+    } 
+
+    
+}
--- Example.Breaker/Scenes/Game/Casting/ActorFactory.cs
+++ Example.Breaker/Scenes/Game/Casting/ActorFactory.cs
@@ -20,8 +20,8 @@ namespace Example.Breaker.Game
             string image = _settingsService.GetString("ballImage");
             float width = _settingsService.GetFloat("ballWidth");
             float height = _settingsService.GetFloat("ballHeight");
-            float x = _settingsService.GetFloat("ballX");
-            float y = _settingsService.GetFloat("ballY");
+            float x = _settingsService.GetFloat(-10);
+            float y = _settingsService.GetFloat(-10);
             float directionX = _settingsService.GetFloat("ballVelocity");
             float directionY = directionX *= -1;
 
